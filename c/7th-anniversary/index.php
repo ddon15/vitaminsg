8b5b175bcc88a376aeffb394b36b49a14a6577fb
Diovannie Donayre <van_van152004@yahoo.com>
2015-11-17 09:41:03 +0800

    .gitignore is now working

--- c/7th-anniversary/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	
-	if (date("Y-m-d") > '2015-04-19') {
-		header("Location: http://www.vitamin.sg/c/offer-ended");
-	}
-	else {
-		header("Location: http://www.vitamin.sg/index.php?route=premium_member/complimentary");		
-	}
--- vidaylin-1for1/index.php
+++
@@ -1,3 +0,0 @@
-<?php 
-	//[KA]
-	header("location:http://www.vitamin.sg/vidaylin-minibear-gummies-60-gummies-vidaylin-minibear-gummies-60-gummies");

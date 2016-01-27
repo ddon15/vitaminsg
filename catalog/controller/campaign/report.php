0c40824347ec64764c3d0b9dd8b75d5805bb42f0
JOAN VILLARIAZA <joan.villariaza@chromedia.com>
2016-01-27 22:01:20 +0800

    Apply temporary solution to the authentication error

--- catalog/controller/campaign/report.php
+++ catalog/controller/campaign/report.php
@@ -9,10 +9,10 @@ class ControllerCampaignReport extends Controller {
 		$query = "SELECT r.referrer, r.name1 AS name, r.email1 AS ref_email, a.* FROM refers r 
 			LEFT JOIN referral_shipping_addresses a ON r.email1 = a.email
 			union all
-			SELECT r.referrer, r.name2, r.email2 AS ref_email, a.* FROM vit.refers r 
+			SELECT r.referrer, r.name2, r.email2 AS ref_email, a.* FROM refers r 
 			LEFT JOIN referral_shipping_addresses a ON r.email2 = a.email
 			union all
-			SELECT r.referrer, r.name3, r.email3 AS ref_email, a.* FROM vit.refers r 
+			SELECT r.referrer, r.name3, r.email3 AS ref_email, a.* FROM refers r 
 			LEFT JOIN referral_shipping_addresses a ON r.email3 = a.email";
 		$referrer = $this->db->query($query);
 

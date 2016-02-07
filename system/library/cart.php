7bf333d3aba7be069c8a59e4343283255eb5af67
Diovannie Donayre <van_van152004@yahoo.com>
2016-02-07 17:23:15 +0800

    Add getproduct cart logic to prioritize discount pricing

--- system/library/cart.php
+++ system/library/cart.php
@@ -215,6 +215,11 @@ class Cart {
 						$price = $product_special_query->row['price'];
 					}
 
+					// Prioritize discount pricing
+					if ($product_special_query->num_rows && $product_discount_query->num_rows) {
+						$price = $product_discount_query->row['price'];
+					}
+
 					//[SB] Check if product is on sale
 					$is_on_sale_query = $this->db->query("SELECT 1 FROM " . DB_PREFIX . "product_special WHERE product_id = '" . (int)$product_id . "' AND customer_group_id = '" . (int)$this->config->get('config_customer_group_id') . "' AND ((date_start = '0000-00-00' OR date_start < NOW()) AND (date_end = '0000-00-00' OR date_end > NOW())) LIMIT 1");
 		

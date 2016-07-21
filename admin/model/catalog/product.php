277b3eccf3760b95b23aabe61c7d0be4964bf7e1
ddon15 <diovannie@cleverlocal.com.au>
2016-07-21 16:29:16 +0800

    uncomment checking of product brand on promotion

--- admin/model/catalog/product.php
+++ admin/model/catalog/product.php
@@ -132,7 +132,7 @@ class ModelCatalogProduct extends Model {
 			}
 		} 
 
-		// $this->checkProductBrandPromotion($data['manufacturer_id'], $product_id);
+		$this->checkProductBrandPromotion($data['manufacturer_id'], $product_id);
 
 		$this->cache->delete('product');
 	}
@@ -293,7 +293,7 @@ class ModelCatalogProduct extends Model {
 
 		$this->db->query("DELETE FROM `" . DB_PREFIX . "product_profile` WHERE product_id = " . (int)$product_id);		if (isset($data['product_profiles'])) {			foreach ($data['product_profiles'] as $profile) {				$this->db->query("INSERT INTO `" . DB_PREFIX . "product_profile` SET `product_id` = " . (int)$product_id . ", customer_group_id = " . (int)$profile['customer_group_id'] . ", `profile_id` = " . (int)$profile['profile_id']);			}		}		$this->cache->delete('product');
 		
-		// $this->checkProductBrandPromotion($data['manufacturer_id'], $product_id);
+		$this->checkProductBrandPromotion($data['manufacturer_id'], $product_id);
 	}
 
 	public function copyProduct($product_id) {

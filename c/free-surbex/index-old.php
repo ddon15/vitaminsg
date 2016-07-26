f241d7c6bf1d76ba82f9cda0d71b66ef7d066f23
Diovannie Donayre <diovannie@cleverlocal.com.au>
2016-07-26 16:53:07 +0800

    Added social media button image

--- c/free-surbex/index-old.php
+++ c/free-surbex/index-old.php
@@ -449,11 +449,23 @@ ENDDOC;
 
 <script type="text/javascript">
 	document.getElementById('share-btn').onclick = function() {
-	  FB.ui({
-	    method: 'share',
-	    display: 'popup',
-	    href: 'https://developers.facebook.com/docs/',
-	  }, function(response){});
+	FB.ui(
+	{
+	  method: 'feed',
+	  name: 'Facebook Dialogs',
+	  link: 'https://developers.facebook.com/docs/dialogs/',
+	  picture: 'http://fbrell.com/f8.jpg',
+	  caption: 'Reference Documentation',
+	  description: 'Dialogs provide a simple, consistent interface for applications to interface with users.'
+	},
+	function(response) {
+	  if (response && response.post_id) {
+	    alert('Post was published.');
+	  } else {
+	    alert('Post was not published.');
+	  }
+	}
+	);
 	}
 </script>
 </body>

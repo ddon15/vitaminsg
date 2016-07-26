dc2a96c7b9a98f54fac71377d672fa6e3320099e
Diovannie Donayre <diovannie@cleverlocal.com.au>
2016-07-26 16:49:30 +0800

    Added social media button image

--- c/free-surbex/index-old.php
+++ c/free-surbex/index-old.php
@@ -420,7 +420,26 @@ ENDDOC;
 </div>	
 
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
+
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '648956708589658',
+      xfbml      : true,
+      version    : 'v2.7'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
+<script>(function(d, s, id) {d
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;

15b55b5f6cb24f8b04f5abdcca237d6985caf5c1
Diovannie Donayre <van_van152004@yahoo.com>
2016-08-02 04:11:33 +0800

    Added brand_promotions language php file.

---
+++ admin/language/english/sale/brand_promotions.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']     = 'Brand Promotions';
+
+// Text
+$_['text_install']      = 'Install';
+$_['text_uninstall']    = 'Uninstall';
+
+$_['button_save']     = 'Save';
+$_['button_cancel']     = 'Cancel';
+$_['button_add_bp']     = 'New';
+$_['button_remove']     = 'Remove';
+
+$_['entry_customer_group']     = 'Customer Group';
+$_['entry_brands']     = 'Brand';
+$_['entry_discount']     = 'Discount (%)';
+$_['entry_priority']     = 'Priority';
+$_['entry_discount_value']     = 'Discount Value';
+$_['entry_date_start']     = 'Date Start';
+$_['entry_date_end']     = 'Date End';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify brand promotion!';
+?>

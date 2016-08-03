6785850bf0598ad51524998fc71c1583418c3655
Diovannie Donayre <diovannie@cleverlocal.com.au>
2016-08-03 17:47:50 +0800

    Free Surbex: Fixes

--- c/free-surbex/sendemail.php
+++ c/free-surbex/sendemail.php
@@ -15,11 +15,15 @@ function sendMail($to) {
 	$subject = " Do you want protection from Free Radical Damage too?";
 	$from =  $senderName . " " . "<" . $_POST['sender_email'] .">";
 	$message = <<<ENDDOC
-<p>Hi friend, </p> <br /> <br /> 
+<!DOCTYPE html>
+<html>
+<body>
+<div>
+<p>Hi friend, </p> <br />
 
 <p>
 	I just got a free bottle of Abbott's Surbex Natopherol Â® Vegicaps from <a href="https://www.vitamin.sg">Vitamin.sg</a> and thought you might want one too. Takes less than 5 minutes to claim a completely free bottle!
-</p> <br />
+</p>
 <p>
 	Surbex Natopherol is a Natural Vitamin E supplement for adults that provides protection against free radical damage. Research has shown that high exposure to free radicals causes or accelerates nerve cell injury and leads to disease. Free Radical Damage may be caused by naturally-occurring radioactive materials found in<br/>
 	<ul>
@@ -28,22 +32,25 @@ function sendMail($to) {
 		<li>and the environment. </li>
 	</ul>
 </p>
-</br>
+
 <p>
 	Common sources are fried foods, medicines, alcohol, tobacco smoke, pesticides, air pollutants, and ionizing radiation-causing electronics. 
 </p>
-<br />
+
 <p>
 	Usually, this supplement sells at $37.20 but Vitamin.sg and Abbott have partnered to give it to you at no cost so just try lah! 
 </p>
-<br />
-<p>
-	Get a bottle of Abbott’s Surbex Natopherol ® Vegicaps <a href="https://www.vitamin.sg/c/free-surbex">here.</a>
-</p> 
-</br> <br/>
 
-<p>Stay Happy and Healthy always, </p><br>
-<p>$senderName</p>
+<p>
+	Get a bottle of Abbott’s Surbex Natopherol ® Vegicaps <a href="https://www.vitamin.sg/c/free-surbex"><b>here.</b></a>
+</p> 
+</br> <br/> <br/>
+
+<p>Stay Happy and Healthy always, </p> <br/>
+<b>$senderName</b>
+</div>
+</body>
+</html>
 ENDDOC;
 	
 	$headers = "MIME-Version: 1.0" . "\r\n";

d343a31522e7b906dddb3e4d82c33c2873a1bd7a
Diovannie Donayre <diovannie@cleverlocal.com.au>
2016-08-03 08:58:45 +0800

    Free Surbex: Fixes

--- c/free-surbex/index.php
+++ c/free-surbex/index.php
@@ -347,100 +347,91 @@
   ga('send', 'pageview');
 </script>
 
-<div id="fb-root"></div>
 <script>
+	// This is called with the results from from FB.getLoginStatus().
+	function statusChangeCallback(response) {
+		console.log('statusChangeCallback');
+		console.log(response);
+		// The response object is returned with a status field that lets the
+		// app know the current login status of the person.
+		// Full docs on the response object can be found in the documentation
+		// for FB.getLoginStatus().
+		if (response.status === 'connected') {
+		  // Logged into your app and Facebook.
+		  testAPI();
+		} else if (response.status === 'not_authorized') {
+		 	// The person is logged into Facebook, but not your app.
+		 	console.log('Not Authorized');
+		} else {
+		  	// The person is not logged into Facebook, so we're not sure if
+		  	// they are logged into this app or not.
+			console.log('No fb user login');
+		}
+	}
+
+	// This function is called when someone finishes with the Login
+	// Button.  See the onlogin handler attached to it in the sample
+	// code below.
+	function checkLoginState() {
+		FB.getLoginStatus(function(response) {
+		  statusChangeCallback(response);
+		});
+	}
+
 	window.fbAsyncInit = function() {
 		FB.init({
-			appId      : '648956708589658', // App ID
-			status     : true, // check login status
-			cookie     : true, // enable cookies to allow the server to access the session
-			xfbml      : true  // parse XFBML
-		});
+		appId      : '648956708589658',
+		cookie     : true,  // enable cookies to allow the server to access 
+		                    // the session
+		xfbml      : true,  // parse social plugins on this page
+		version    : 'v2.5' // use graph api version 2.5
+	});
 
-		FB.Event.subscribe('auth.authResponseChange', function(response) {
-			console.log(response);
-			if (response.status === 'connected') {
-				console.log('connected');
-				testAPI();
-			} else if (response.status === 'not_authorized') {
-				console.log('not authorized');
-				fbLogin();
-			} else {
-				console.log('not login');
-				fbLogin();
-			}
-		})
+	// Now that we've initialized the JavaScript SDK, we call 
+	// FB.getLoginStatus().  This function gets the state of the
+	// person visiting this page and can return one of three states to
+	// the callback you provide.  They can be:
+	//
+	// 1. Logged into your app ('connected')
+	// 2. Logged into Facebook, but not your app ('not_authorized')
+	// 3. Not logged into Facebook and can't tell if they are logged into
+	//    your app or not.
+	//
+	// These three cases are handled in the callback function.
 
-		var page_like_or_unlike_callback = function(url, html_element) {
-		  console.log("page_like_or_unlike_callback");
-		  console.log(url);
-		  console.log(html_element);
-		}
+	FB.getLoginStatus(function(response) {
+		statusChangeCallback(response);
+	});
 
-		// In your onload handler
-		FB.Event.subscribe('edge.create', page_like_or_unlike_callback);
 	};
 
 	// Load the SDK asynchronously
-	(function(d) {
-	 var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-	 if (d.getElementById(id)) {return;}
-	 js = d.createElement('script'); js.id = id; js.async = true;
-	 js.src = "//connect.facebook.net/en_US/all.js";
-	 ref.parentNode.insertBefore(js, ref);
-	}(document));
+	(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js";
+	fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
 
+	// Here we run a very simple test of the Graph API after login is
+	// successful.  See statusChangeCallback() for when this call is made.
 	function testAPI() {
-	  FB.api('/me', function(response) {
-	    console.log('Current Fb Login User, ' + response.name + '.');
-	  });
-	  FB.api('/me/likes/164602346987323', function(response) {
-	    console.log(response.data);
-	    if (response.data) {
-	    	$("#fb-like-container").data("liked", "1");
-	    }
-	  });
-	}
-
-	function fbLogin() {
-		FB.login(function(response) {
-		  if (response.authResponse) {
-		      FB.api('/me/likes/164602346987323', function(response) {
-			    console.log(response.data);
-			    if (response.data) {
-			    	$("#fb-like-container").data("liked", "1");
-			    }
-			  });
-		     } else {
-		     	$("#fb-like-container").data("liked", "0");
-		      	console.log('User cancelled login or did not fully authorize.');
-		     }
-		}, {scope: 'email, user_likes'});
-	}
-
-	document.getElementById('share-btn').onclick = function(e) {
-		FB.ui(
-		{
-		  method: 'feed',
-		  name: 'Offer',
-		  link: 'https://www.vitamin.sg/c/free-surbex',
-		  href: 'https://www.facebook.com/vitaminsg/posts/1010990432348506',
-		  // picture: 'http://fbrell.com/f8.jpg',
-		  // caption: 'Reference Documentation',
-		  // description: 'Dialogs provide a simple, consistent interface for applications to interface with users.'
-		},
-		function(response) {
-		 	if (response && response.post_id) {
-		  		$("#fb-shared-container").data("shared", "1");
-		   		console.log('Post was published');
-		  } else {
-		  		console.log('Post was not published');
-		  }
+		console.log('Welcome!  Fetching your information.... ');
+		FB.api('/me', function(response) {
+		  console.log('Successful login for: ' + response.name);
 		});
 
-		e.preventDefault();
+		FB.api('/me/likes/164602346987323', function(response) {
+		  console.log(response.data);
+		  if (response.data) {
+		  	$("#fb-like-container").data("liked", "1");
+		  }
+		});
 	}
+</script>
 
+<script type="text/javascript">
 	$(document).ready(function(e) {
 		$('#form-reg').on('submit', function(e) {
 
@@ -459,6 +450,29 @@
 
 			$(this).submit();
 
+			e.preventDefault();
+		});
+
+		$('#share-btn').on('click', function(e) {
+			FB.ui(
+			{
+				method: 'feed',
+				name: 'Offer',
+				link: 'https://www.vitamin.sg/c/free-surbex',
+				href: 'https://www.facebook.com/vitaminsg/posts/1010990432348506',
+				// picture: 'http://fbrell.com/f8.jpg',
+				// caption: 'Reference Documentation',
+				// description: 'Dialogs provide a simple, consistent interface for applications to interface with users.'
+			},
+			function(response) {
+			 	if (response && response.post_id) {
+			  		$("#fb-shared-container").data("shared", "1");
+			   		console.log('Post was published');
+			  } else {
+			  		console.log('Post was not published');
+			  }
+			});
+			
 			e.preventDefault();
 		});
 
